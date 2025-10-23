85bceb90895ec3d208fe773e9c4c32b3ee00cd8e
VictorGotsenko <victor.gotsenko@gmail.com>
2025-10-23 17:27:20 +0500

    Edit test2

--- src/test/java/hexlet/code/controller/TestUserController.java
+++ src/test/java/hexlet/code/controller/TestUserController.java
@@ -1,16 +1,13 @@
 package hexlet.code.controller;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
 public class TestUserController {
 
     @Test
-    public void testWelcome() throws Exception {
+    public void testWelcome() {
         boolean actualCondition = true;
         assertTrue(actualCondition); // This test will pass
 
