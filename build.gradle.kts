53ed2f31eb5a96de3b8daa8c03829dbce8a5cb9a
VictorGotsenko <victor.gotsenko@gmail.com>
2025-11-24 19:05:34 +0500

    Add path repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ java {
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
